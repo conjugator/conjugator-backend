ce7246f298fadafb57332490fe2604928ea2829c
Ashley Owens <ash4741@gmail.com>
2019-04-29 22:55:42 -0400

    Heroku

--- config/users-module.js
+++ config/users-module.js
@@ -7,18 +7,18 @@ module.exports = {
 };
 
 function findBy(filter) {
-  return db('users').where(filter);
+  return db("users").where(filter);
 }
 
 async function add(user) {
-  const [id] = await db('users').insert(user, ["id"]);
+  const [id] = await db("users").insert(user, ["id"]);
 
   return findById(id);
 }
 
 function findById(id) {
-  return db('users')
-    .select('id', 'username')
+  return db("users")
+    .select("id", "username")
     .where({ id })
     .first();
 }
